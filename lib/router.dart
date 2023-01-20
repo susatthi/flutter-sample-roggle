5686f4e7fbb8bdbe1884a712150533e069c690b5
susa.keyber <susa@keyber.jp>
2023-01-20 13:44:00 +0900

    🚀 feat:

--- lib/router.dart
+++ lib/router.dart
@@ -26,111 +26,25 @@ class FirstRoute extends GoRouteData {
   const FirstRoute();
 
   @override
-  Page<void> buildPage(BuildContext context) => TransitionPage.slide(
-        name: 'first',
-        child: const FirstScreen(),
-      );
+  Widget build(BuildContext context) {
+    return const FirstScreen();
+  }
 }
 
 class SecondRoute extends GoRouteData {
   const SecondRoute();
 
   @override
-  Page<void> buildPage(BuildContext context) => TransitionPage.slide(
-        name: 'second',
-        child: const SecondScreen(),
-      );
+  Widget build(BuildContext context) {
+    return const SecondScreen();
+  }
 }
 
 class ThirdRoute extends GoRouteData {
   const ThirdRoute();
 
   @override
-  Page<void> buildPage(BuildContext context) => TransitionPage.slide(
-        name: 'third',
-        child: const ThirdScreen(),
-      );
-}
-
-/// デフォルトのTransitionPage
-class TransitionPage extends CustomTransitionPage<void> {
-  const TransitionPage({
-    required String name,
-    required super.child,
-    required super.transitionsBuilder,
-    super.transitionDuration,
-    super.maintainState,
-    super.fullscreenDialog,
-    super.opaque,
-    super.barrierDismissible,
-    super.barrierColor,
-    super.barrierLabel,
-    super.key,
-    super.arguments,
-    super.restorationId,
-  }) : super(
-          name: name,
-        );
-
-  factory TransitionPage.fade({
-    required Widget child,
-    required String name,
-  }) =>
-      TransitionPage(
-        name: name,
-        child: child,
-        transitionsBuilder: _fadeTransitionsBuilder,
-      );
-
-  factory TransitionPage.slide({
-    required Widget child,
-    required String name,
-  }) =>
-      TransitionPage(
-        name: name,
-        child: child,
-        transitionsBuilder: _slideTransitionsBuilder,
-      );
-
-  factory TransitionPage.none({
-    required Widget child,
-    required String name,
-  }) =>
-      TransitionPage(
-        name: name,
-        child: child,
-        transitionsBuilder: _noneTransitionsBuilder,
-      );
-
-  static Widget _slideTransitionsBuilder(
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-    Widget child,
-  ) =>
-      SlideTransition(
-        position: Tween<Offset>(
-          begin: const Offset(1, 0),
-          end: Offset.zero,
-        ).animate(
-          CurvedAnimation(parent: animation, curve: Curves.fastOutSlowIn),
-        ),
-        child: child,
-      );
-
-  static Widget _fadeTransitionsBuilder(
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-    Widget child,
-  ) =>
-      FadeTransition(opacity: animation, child: child);
-
-  static Widget _noneTransitionsBuilder(
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-    Widget child,
-  ) =>
-      child;
+  Widget build(BuildContext context) {
+    return const ThirdScreen();
+  }
 }
